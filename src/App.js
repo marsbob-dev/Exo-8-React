a96a0a4dad55490b43116021b725d2711070e28c
marsbob-dev <73162318+marsbob-dev@users.noreply.github.com>
2021-01-19 10:43:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+
+  onClick = () => {
+    alert('Hello World !')
+  }
+
+  render() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Exercice 8</h1>
+      <button onClick={this.onClick}>BTN</button>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
