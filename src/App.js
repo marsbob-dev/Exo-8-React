4d235f565c20cb9a7f423cea15c35816ded4d82b
marsbob-dev <73162318+marsbob-dev@users.noreply.github.com>
2021-01-19 10:50:41 +0100

    exo terminé

--- src/App.js
+++ src/App.js
@@ -7,11 +7,18 @@ class App extends Component {
   onClick = () => {
     alert('Hello World !')
   }
+  onMouseOver = (e) => {
+    console.log(e.target.innerHTML)
+  }
 
+  state = {
+    nombre: "1"
+  }
   render() {
+    console.log(this.state.nombre)
     return (
     <div className="App">
-      <h1>Exercice 8</h1>
+      <h1 onMouseOver={this.onMouseOver}>Exercice 8</h1>
       <button onClick={this.onClick}>BTN</button>
     </div>
     )
